9db86545a7c79a9f15082ddadfe948b953ddcf47
Humbertico04 <humbertico2004@gmail.com>
2023-03-23 09:46:05 +0100

    test set up

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,3 +1,9 @@
 import unittest
 import database as db
 
+class Test_database(unittest.TestCase):
+    def set_up(self):
+        self.a = db.Punto(2, 3)
+        self.b = db.Punto(5, 5)
+        self.c = db.Punto(-3, -1)
+        self.d = db.Punto(0, 0)
