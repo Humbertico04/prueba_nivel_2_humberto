c97dc0f2199391c90577be51ce91bb00d2984f9e
Humbertico04 <humbertico2004@gmail.com>
2023-03-23 09:52:21 +0100

    fixeo tests

--- tests/test_database.py
+++ tests/test_database.py
@@ -2,19 +2,18 @@ import unittest
 import database as db
 
 class Test_database(unittest.TestCase):
-    def set_up(self):
+    def setUp(self):
         self.a = db.Punto(2, 3)
         self.b = db.Punto(5, 5)
         self.c = db.Punto(-3, -1)
         self.d = db.Punto(0, 0)
 
     def test_cuadrante(self):
-        self.assertequal(self.a.cuadrante(), "El punto (2, 3) está situado en el primer cuadrante")
-        self.assertequal(self.b.cuadrante(), "El punto (5, 5) está situado en el primer cuadrante")
-        self.assertequal(self.c.cuadrante(), "El punto (-3, -1) está situado en el tercer cuadrante")
-        self.assertequal(self.d.cuadrante(), "El punto (0, 0) está situado sobre el origen")
+        self.assertEqual(self.a.cuadrante(), "El punto (2, 3) está situado en el primer cuadrante")
+        self.assertEqual(self.b.cuadrante(), "El punto (5, 5) está situado en el primer cuadrante")
+        self.assertEqual(self.c.cuadrante(), "El punto (-3, -1) está situado en el tercer cuadrante")
+        self.assertEqual(self.d.cuadrante(), "El punto (0, 0) está situado sobre el origen")
 
     def test_vector(self):
-        self.assertequal(self.a.vector(self.b), "El vector que va de (2, 3) --> (5, 5) = (3, 2)")
-        self.assertequal(self.b.vector(self.a), "El vector que va de (5, 5) --> (2, 3) = (-3, -2)")
-        
+        self.assertEqual(self.a.vector(self.b), "El vector que va de (2, 3) --> (5, 5) = (3, 2)")
+        self.assertEqual(self.b.vector(self.a), "El vector que va de (5, 5) --> (2, 3) = (-3, -2)")
