ab185cee620d2d3e4c2822ea8477f0f95223597e
Humbertico04 <humbertico2004@gmail.com>
2023-03-23 09:33:10 +0100

    inicio exp

---
+++ experimentación.py
@@ -0,0 +1,16 @@
+# Crea los puntos A(2, 3), B(5,5), C(-3, -1) y D(0,0) e imprimelos por
+# pantalla.
+# • Consulta a que cuadrante pertenecen el punto A, C y D.
+# • Consulta los vectores AB y BA.
+# • (Optativo) Consulta la distancia entre los puntos 'A y B' y 'B y A'.
+# • (Optativo) Determina cual de los 3 puntos A, B o C, se encuentra más
+# lejos del origen, punto (0,0).
+# • Crea un rectángulo utilizando los puntos A y B.
+# • Consulta la base, altura y área del rectángulo.
+
+import database as db
+
+a = db.Punto(2, 3)
+b = db.Punto(5, 5)
+c = db.Punto(-3, -1)
+d = db.Punto(0, 0)
